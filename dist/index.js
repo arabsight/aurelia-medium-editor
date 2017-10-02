8d312ed2eae9cab33004d5f44a3b6b65800a331f
Rabah Ghodbane <r-ghodbane@live.com>
2017-10-02 14:46:19 +0100

    Mon Oct  2 14:46:19 CET 2017

--- dist/index.js
+++ dist/index.js
@@ -3,32 +3,30 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.MediumEditor = exports.default = undefined;
+exports.MediumEditorPlugin = exports.MediumEditor = undefined;
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 var _mediumEditor = require('./medium-editor');
 
-Object.defineProperty(exports, 'default', {
+Object.defineProperty(exports, 'MediumEditor', {
     enumerable: true,
     get: function get() {
-        return _interopRequireDefault(_mediumEditor).default;
+        return _mediumEditor.MediumEditor;
     }
 });
 
 var _mediumEditorPlugin = require('./medium-editor-plugin');
 
-Object.defineProperty(exports, 'MediumEditor', {
+Object.defineProperty(exports, 'MediumEditorPlugin', {
     enumerable: true,
     get: function get() {
-        return _mediumEditorPlugin.MediumEditor;
+        return _mediumEditorPlugin.MediumEditorPlugin;
     }
 });
 exports.configure = configure;
 
 var _aureliaPal = require('aurelia-pal');
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function configure(config, editorConfig) {
     if ((typeof editorConfig === 'undefined' ? 'undefined' : _typeof(editorConfig)) === 'object') {
